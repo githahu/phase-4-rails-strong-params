3dfcc9c1236d627eb181eb4c38d32004d278d550
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-29 02:31:40 +0300

    assignment done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
-
+  wrap_parameter format: []
+end
   # GET /birds
   def index
     birds = Bird.all
@@ -8,7 +9,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +21,10 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+  private
+  def bird_params
+    params.(:name, :species)
   end
 
 end
